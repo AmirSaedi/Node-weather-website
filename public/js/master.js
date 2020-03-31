44b17aa1638a67729df094bf8c4774fa74252bcf
Amir <amirhosseinsaedi@hotmail.com>
2020-04-01 02:10:35 +0430

    Setup heroku config

--- public/js/master.js
+++ public/js/master.js
@@ -6,18 +6,16 @@ document
   .addEventListener("click", event => {
     event.preventDefault();
     const getInputValue = formInput.value;
-    fetch(`http://localhost:3000/weather?address=${getInputValue}`).then(
-      response => {
-        response.json().then(data => {
-          if (data.error) {
-            pushData.innerHTML = data.error;
-          } else {
-            pushData.innerHTML = `<h4>Your selected location: ${data.location}</h4>
+    fetch(`/weather?address=${getInputValue}`).then(response => {
+      response.json().then(data => {
+        if (data.error) {
+          pushData.innerHTML = data.error;
+        } else {
+          pushData.innerHTML = `<h4>Your selected location: ${data.location}</h4>
             <p>Forecast summary: ${data.forecast.summary}</p>
             <div>Temperature: ${data.forecast.temperature}</div>
             <div>Chance of rain: ${data.forecast.chanceOfRain}%</div>`;
-          }
-        });
-      }
-    );
+        }
+      });
+    });
   });
