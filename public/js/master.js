fef27a48c1ba0ff8856b54e14b9b36a6e2971fab
Amir <amirhosseinsaedi@hotmail.com>
2020-03-31 23:20:55 +0430

    Fixed master.js

--- public/js/master.js
+++ public/js/master.js
@@ -1,18 +1,23 @@
 const pushData = document.getElementById("weather-wrapper");
 const formInput = document.querySelector("#weather-form>input");
-document.querySelector("#weather-form>button").addEventListener("click", (event) => {
+
+document
+  .querySelector("#weather-form>button")
+  .addEventListener("click", event => {
     event.preventDefault();
     const getInputValue = formInput.value;
-    fetch(`http://localhost:3000/weather?address=${getInputValue}`).then((response) => {
-        response.json().then((data) => {
-            if (data.error) {
-                pushData.innerHTML = data.error;
-            } else {
-                pushData.innerHTML = `<h4>Your selected location: ${data.location}</h4>
+    fetch(`http://localhost:3000/weather?address=${getInputValue}`).then(
+      response => {
+        response.json().then(data => {
+          if (data.error) {
+            pushData.innerHTML = data.error;
+          } else {
+            pushData.innerHTML = `<h4>Your selected location: ${data.location}</h4>
             <p>Forecast summary: ${data.forecast.summary}</p>
             <div>Temperature: ${data.forecast.temperature}</div>
             <div>Chance of rain: ${data.forecast.chanceOfRain}%</div>`;
-            }
+          }
         });
-    });
-});
+      }
+    );
+  });
